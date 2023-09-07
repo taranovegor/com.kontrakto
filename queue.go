33e8507b887529d9ffd5c3b07d3c68842f9670d3
Egor Taranov <dev@taranovegor.com>
2023-09-07 21:38:06 +0200

    tec-20: added queue configuration for ligilo application

--- queue.go
+++ queue.go
@@ -4,3 +4,19 @@ const (
 	QueueLigilo       = "ligilo"
 	QueueNotification = "notification"
 )
+
+type QueueProducers map[interface{}][]string
+
+type QueueConfig struct {
+	Producers QueueProducers
+}
+
+func GetQueueConfig() QueueConfig {
+	return QueueConfig{
+		Producers: QueueProducers{
+			CreateShortLink{}: {QueueLigilo},
+			UpdateShortLink{}: {QueueLigilo},
+			DeleteShortLink{}: {QueueLigilo},
+		},
+	}
+}
